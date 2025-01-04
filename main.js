c72012e98af48d09cf62caf5e01a0e5da0609fcf
yeaeun23 <yeaeun23@naver.com>
2025-01-04 15:12:57 +0900

    버튼 스타일, 클릭 애니메이션

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ const btnEl = document.querySelector(".btn");
 
 btnEl.addEventListener("click", () => {
   btnEl.classList.add("loading");
+
+  setTimeout(() => {
+    btnEl.classList.remove("loading");
+  }, 3000); // 3초
 });
